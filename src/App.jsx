77f9d53340b497f1d3eafd58c804664896b4b278
Kevin Rodrigues <kevinrodrigues43@gmail.com>
2024-04-07 16:38:52 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import ExpenseCTA from './components/ExpenseCTA.jsx'
+
+
+function App() {
+  
+  return (
+    <>
+      <Header />
+      <ExpenseCTA />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseCTA.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function ExpenseCTA() {
+  return (
+<>
+    
+        <div className='w-20 h-20'>
+        <button className=" join-item btn btn-circle">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-20 w-20" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+  Add Expense
+</button>
+        </div>
+   
+<button className="join-item btn btn-circle btn-outline">
+  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+  Top Up
+</button>
+ 
+</>
+
+  )
+}
